39522efbce186262e92546afa629662dcb2daa4d
nur <nurxlan@gmail.com>
2023-07-27 23:11:05 +0200

    gen dyck strings

--- generate_dataset.py
+++ generate_dataset.py
@@ -4,9 +4,11 @@ import math
 import pathlib
 import random
 import subprocess
-from typing import Optional
 
+import nltk
 import numpy as np
+from nltk.parse import generate
+from tqdm import tqdm
 
 _DEFAULT_SEED = 100
 _DEFAULT_PRIOR = 0.3
@@ -19,6 +21,11 @@ _BATCH_SIZES = (
 _PREVIEW_SIZE = 10
 _NUM_TEST_STRINGS = 15_000
 _ZIP_PASSWORD = "1234"
+
+_DYCK_BRACKET_PAIRS = (
+    ("(", ")"),
+    ("[", "]"),
+)
 
 
 def _get_language_path(language_name) -> pathlib.Path:
@@ -74,9 +81,7 @@ class _DerivationTooLong(Exception):
     pass
 
 
-def _generate_string_from_pcfg(
-    pcfg: dict, max_length: Optional[int] = None
-) -> tuple[str, ...]:
+def _generate_string_from_pcfg(pcfg: dict) -> str:
     """
     Example:
     ```
@@ -100,8 +105,6 @@ def _generate_string_from_pcfg(
         if node not in pcfg:
             if node != "":
                 terminals.append(node)
-            if max_length is not None and len(terminals) > max_length:
-                raise _DerivationTooLong
             continue
 
         rules, probabs = list(zip(*pcfg[node]))
@@ -110,32 +113,34 @@ def _generate_string_from_pcfg(
 
         stack = list(rule) + stack
 
-    return tuple(terminals)
+    return "".join(terminals)
 
 
 def make_corpus_from_pcfg(
-    pcfg: dict, batch_size: int, max_length: Optional[int] = None
+    pcfg: dict, batch_size: int, add_start_end_of_sequence: bool
 ) -> tuple[str, ...]:
     sequences = []
     while len(sequences) < batch_size:
-        try:
-            sequence = _generate_string_from_pcfg(pcfg, max_length=max_length)
-        except _DerivationTooLong:
-            continue
-        sequences.append("".join(sequence))
+        s = _generate_string_from_pcfg(pcfg)
+        if add_start_end_of_sequence:
+            s = f"#{s}#"
+        sequences.append(s)
     return tuple(sorted(sequences, key=len))
 
 
 def _gen_an_bn_training_strings(batch_size, prior):
     an_bn_pcfg = {
-        "S": ((("#", "a", "X", "b", "#"), 1),),
+        # n > 0.
+        "S": ((("a", "X", "b"), 1),),
         "X": (
             (("a", "X", "b"), 1 - prior),
             (("",), prior),
         ),
     }
     return make_corpus_from_pcfg(
-        batch_size=batch_size, pcfg=an_bn_pcfg, max_length=None
+        batch_size=batch_size,
+        pcfg=an_bn_pcfg,
+        add_start_end_of_sequence=True,
     )
 
 
@@ -154,7 +159,8 @@ def _make_an_bn_etc_deterministic_steps_mask(strings) -> np.ndarray:
 
 def gen_an_bn(prior, seed):
     for batch_size in _BATCH_SIZES:
-        training_strings = tuple(_gen_an_bn_training_strings(batch_size, prior))
+        _seed(seed)
+        training_strings = _gen_an_bn_training_strings(batch_size, prior)
         _write_strings(
             training_strings,
             language_name="an_bn",
@@ -217,7 +223,8 @@ def _gen_an_bn_cn_etc_strings(n_values, language_name):
 
 def _gen_an_bm_c_n_plus_m_training_strings(batch_size, prior):
     pcfg = {
-        "S": ((("#", "a", "X", "c", "#"), 1),),
+        # n > 0.
+        "S": ((("a", "X", "c"), 1),),
         "X": (
             (("a", "X", "c"), 1 - prior),
             (("Y",), prior),
@@ -228,7 +235,9 @@ def _gen_an_bm_c_n_plus_m_training_strings(batch_size, prior):
             (("",), prior),
         ),
     }
-    return make_corpus_from_pcfg(batch_size=batch_size, pcfg=pcfg, max_length=None)
+    return make_corpus_from_pcfg(
+        batch_size=batch_size, pcfg=pcfg, add_start_end_of_sequence=True
+    )
 
 
 def _make_an_bm_c_n_plus_m_deterministic_steps_mask(strings) -> np.ndarray:
@@ -247,6 +256,7 @@ def _make_an_bm_c_n_plus_m_deterministic_steps_mask(strings) -> np.ndarray:
 
 def gen_an_bm_c_n_plus_m(prior, seed):
     for batch_size in _BATCH_SIZES:
+        _seed(seed)
         training_strings = _gen_an_bm_c_n_plus_m_training_strings(batch_size, prior)
 
         _write_strings(
@@ -298,8 +308,92 @@ def gen_an_bm_c_n_plus_m(prior, seed):
         _zip_and_delete(test_deterministic_steps_mask_path)
 
 
+def _gen_first_dyck_strings(dyck_n: int, num_strings: int):
+    cfg_str = "S -> "
+
+    for open_bracket, close_bracket in _DYCK_BRACKET_PAIRS[:dyck_n]:
+        cfg_str += f'"{open_bracket}" S "{close_bracket}" |'
+
+    cfg_str += " S S | "
+
+    print(cfg_str)
+    cfg = nltk.CFG.fromstring(cfg_str)
+
+    strings = set()
+    progress_bar = tqdm(total=num_strings)
+    for sentence in generate.generate(cfg, depth=200):
+        s = "".join(filter(len, sentence))
+        num_before = len(strings)
+        strings.add(s)
+        num_after = len(strings)
+
+        if len(strings) == num_strings:
+            break
+
+        if num_after - num_before:
+            progress_bar.update(1)
+
+    return tuple(sorted(set(strings), key=len))[:num_strings]
+
+
+def gen_dyck(dyck_n: int, nesting_probab: float, seed: int):
+    single_nesting_probab = nesting_probab / dyck_n
+
+    bracket_derivations = []
+    for i in range(dyck_n):
+        bracket_derivations.append(
+            # `S -> ("[", S, "]", S)`.
+            (
+                (_DYCK_BRACKET_PAIRS[i][0], "S", _DYCK_BRACKET_PAIRS[i][1], "S"),
+                single_nesting_probab,
+            )
+        )
+
+    pcfg = {"S": tuple(bracket_derivations) + (("", 1 - nesting_probab),)}
+
+    language_name = f"dyck_{dyck_n}"
+
+    for batch_size in _BATCH_SIZES:
+        _seed(seed)
+        training_strings = make_corpus_from_pcfg(
+            batch_size=batch_size, pcfg=pcfg, add_start_end_of_sequence=True
+        )
+        _write_strings(
+            training_strings,
+            language_name=language_name,
+            is_test=False,
+            is_preview=False,
+            batch_size=batch_size,
+            prior=nesting_probab,
+            seed=seed,
+            zip=True,
+        )
+
+    # test_strings = _gen_first_dyck_strings(
+    #     dyck_n=dyck_n,
+    #     num_strings=_NUM_TEST_STRINGS,
+    # )
+    #
+    # _write_strings(
+    #     test_strings,
+    #     language_name=language_name,
+    #     is_test=True,
+    #     zip=True,
+    # )
+    #
+    # preview_strings = test_strings[:_PREVIEW_SIZE]
+    # _write_strings(
+    #     preview_strings,
+    #     language_name=language_name,
+    #     is_preview=True,
+    #     is_test=False,
+    #     zip=False,
+    # )
+
+
 def gen_an_bn_cn_etc(language_name, prior, seed):
     for batch_size in _BATCH_SIZES:
+        _seed(seed)
         n_values = sorted(np.random.geometric(p=prior, size=batch_size))
         training_strings = _gen_an_bn_cn_etc_strings(n_values, language_name)
 
@@ -374,8 +468,6 @@ if __name__ == "__main__":
     )
     arguments = arg_parser.parse_args()
 
-    _seed(arguments.seed)
-
     if arguments.language_name == "an_bn":
         gen_an_bn(prior=arguments.prior, seed=arguments.seed)
     elif arguments.language_name in {"an_bn_cn", "an_bn_cn_dn"}:
@@ -389,7 +481,8 @@ if __name__ == "__main__":
             prior=arguments.prior,
             seed=arguments.seed,
         )
-    elif arguments.language_name.startswith("dyck"):
-        pass
+    elif arguments.language_name.startswith("dyck_"):
+        dyck_n = int(arguments.language_name.split("_")[-1])
+        gen_dyck(dyck_n, nesting_probab=arguments.prior, seed=arguments.seed)
     else:
         raise ValueError(arguments.language_name)
